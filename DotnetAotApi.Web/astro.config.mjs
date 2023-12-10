9b2b7ad6df65b92047d1886969f258b22c804272
Ryan Seipp <rseipp@ryanseipp.com>
2023-12-10 15:12:23 -0500

    Fix dockerfile

--- DotnetAotApi.Web/astro.config.mjs
+++ DotnetAotApi.Web/astro.config.mjs
@@ -6,5 +6,5 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
-  outDir: "../DotnetAotApi.Api/wwwroot/"
 });
+
